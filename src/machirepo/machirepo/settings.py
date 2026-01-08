1e0f02a3cfe1c75a99c84e16a49b619b9f588ccd
haruki2024 <harukishogo2226@gmail.com>
2026-01-08 14:50:44 +0900

    メール修正②

--- src/machirepo/machirepo/settings.py
+++ src/machirepo/machirepo/settings.py
@@ -38,7 +38,15 @@ INSTALLED_APPS = [
 # EMAIL_HOST_PASSWORD = 'dnod zifk oimg ambl'
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
+EMAIL_HOST_USER = 'machirepo.app@gmail.com'
+EMAIL_HOST_PASSWORD ='dnod zifk oimg ambl'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 MIDDLEWARE = [
